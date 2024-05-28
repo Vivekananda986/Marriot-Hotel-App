32467dd96920ba5fed2002215d43001952fcf038
Vivek <vivekanandat92@gmail.com>
2024-05-29 00:11:59 +0530

    commit

---
+++ MarriotHotelAppClient/src/components/layout/MainHeader.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const MainHeader = () => {
+	return (
+		<header className="header-banner">
+			<div className="overlay"></div>
+			<div className="animated-texts overlay-content">
+				<h1>
+					Welcome to <span className="hotel-color">Hotel Marriot</span>
+				</h1>
+			</div>
+		</header>
+	)
+}
+
+export default MainHeader
---
+++ MarriotHotelAppServer/src/main/java/com/dailycodework/lakesidehotel/service/UserService.java
@@ -0,0 +1,84 @@
+package com.dailycodework.lakesidehotel.service;
+
+import com.dailycodework.lakesidehotel.exception.UserAlreadyExistsException;
+import com.dailycodework.lakesidehotel.model.Role;
+import com.dailycodework.lakesidehotel.model.User;
+import com.dailycodework.lakesidehotel.repository.RoleRepository;
+import com.dailycodework.lakesidehotel.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class UserService implements IUserService {
+	@Autowired
+    private UserRepository userRepository;
+	@Autowired
+    private  PasswordEncoder passwordEncoder;
+	@Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public User registerUser(User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new UserAlreadyExistsException(user.getEmail() + " already exists");
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        System.out.println(user.getPassword());
+
+        // Handle multiple roles, and assign "ROLE_USER" if no roles are provided
+        Set<Role> userRoles = new HashSet<>();
+        if (user.getRoles() == null || user.getRoles().isEmpty()) {
+            Role userRole = roleRepository.findByName("ROLE_USER")
+                    .orElseGet(() -> {
+                        Role newRole = new Role();
+                        newRole.setName("ROLE_USER");
+                        return roleRepository.save(newRole);
+                    });
+            userRoles.add(userRole);
+        } else {
+            for (Role role : user.getRoles()) {
+                Role existingRole = roleRepository.findByName(role.getName())
+                        .orElseGet(() -> {
+                            Role newRole = new Role();
+                            newRole.setName(role.getName());
+                            return roleRepository.save(newRole);
+                        });
+                userRoles.add(existingRole);
+            }
+        }
+        user.setRoles(userRoles);
+
+        return userRepository.save(user);
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @Transactional
+    @Override
+    public void deleteUser(String email) {
+        User theUser = getUser(email);
+        if (theUser != null){
+            userRepository.deleteByEmail(email);
+        }
+
+    }
+
+    @Override
+    public User getUser(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+
+}
